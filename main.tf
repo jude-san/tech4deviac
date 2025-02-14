b98b1251565bfa393f0ee8ba793d46bd55abe973
jude-san <judekhay@gmail.com>
2025-02-14 16:53:56 +0000

    ci: name best

--- main.tf
+++ main.tf
@@ -30,5 +30,12 @@ resource "null_resource" "example" {
   provisioner "local-exec" {
     command = "echo 'Hello, World!'"
   }
-
 }
+
+resource "null_resource" "name" {
+  provisioner "local-exec" {
+    command = "echo 'Hello, World!'"
+  }
+}
+
+
